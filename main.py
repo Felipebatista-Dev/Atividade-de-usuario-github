b4ea880c58cff55d471343caf208a983722e2ad9
Felipebatista-Dev <felipebatistadev89@gmail.com>
2025-07-01 21:37:51 -0300

    Get
    criei o primeiro código, uma requisição  get simples em que abri o endpoint da api e armazenei em uma variável, em seguida esta variável foi lida com read() e os dado foram armazenadas em outra, por fim dei um print.

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-nu = input("Informe seu nome de usuario no github: ")
+import urllib
+import urllib.request
+# nu = input("Informe seu nome de usuario no github: ")
+
+
+teste = urllib.request.urlopen('https://api.github.com/users/kamranahmedse/events')
+dados = teste.read()
+print(dados)
